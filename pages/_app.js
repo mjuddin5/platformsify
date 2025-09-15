6537d410a2d3e78e96a98865251444b6e04a315f
Meheraj Uddin <mjuddin5@gmail.com>
2025-09-15 20:20:12 +0100

    Wrap all pages with Footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,11 @@
+import Footer from '../components/Footer';
 import '../styles/globals.css';
-import Navbar from '../components/Navbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
