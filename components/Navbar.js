ee30a5c2daac60f4edf9fd7714ec73c68393ed05
Meheraj Uddin <mjuddin5@gmail.com>
2025-09-16 14:56:39 +0100

    Add navigation links to Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,13 +3,16 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="bg-gray-100 p-4 shadow">
-      <ul className="flex justify-center space-x-6">
-        <li><Link href="/">Home</Link></li>
-        <li><Link href="/about">About</Link></li>
-        <li><Link href="/services">Services</Link></li>
-        <li><Link href="/contact">Contact</Link></li>
-      </ul>
+    <nav className="bg-gray-900 text-white px-6 py-4 flex justify-between items-center shadow-md">
+      <div className="text-xl font-bold">
+        <Link href="/">Platformsify</Link>
+      </div>
+      <div className="space-x-6">
+        <Link href="/" className="hover:text-blue-400">Home</Link>
+        <Link href="/about" className="hover:text-blue-400">About</Link>
+        <Link href="/services" className="hover:text-blue-400">Services</Link>
+        <Link href="/contact" className="hover:text-blue-400">Contact</Link>
+      </div>
     </nav>
   );
 }
